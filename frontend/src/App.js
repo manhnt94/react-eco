62eb811401a539c6bc0adb2455b3d515333db0f5
ntienmanh <ntienmanh94@gmail.com>
2021-04-14 11:43:07 +0700

    Features: Add Product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <HomePage />
-      </div>
+      <Header />
+      <Switch>
+        <Route path="/product/:id" component={ProductPage} />
+        <Route path="/" component={HomePage} />
+      </Switch>
     </BrowserRouter>
   );
 }
--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Message(props) {
   return (
-    <div className={`${props.error} ? bg-red-500 : bg-green-400`}>
+    <div className={`${props.error ? "bg-red-500" : "bg-green-400"} `}>
       {props.children}
     </div>
   );
