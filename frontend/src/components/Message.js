433d4fabfa4b039ad7d88a13988253f56e8792db
ntienmanh <ntienmanh94@gmail.com>
2021-04-14 10:16:01 +0700

    Feature: Add Home page

---
+++ frontend/src/components/Message.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Message(props) {
+  return (
+    <div className={`${props.error} ? bg-red-500 : bg-green-400`}>
+      {props.children}
+    </div>
+  );
+}
