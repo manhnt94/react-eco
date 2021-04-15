9fb21baa25907b3168f4b8746c871d62c28cc883
ntienmanh <ntienmanh94@gmail.com>
2021-04-15 10:40:55 +0700

    Features: Add Register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,14 @@ import CartPage from "./pages/CartPage";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import ProductPage from "./pages/ProductPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Switch>
+        <Route path="/register" component={RegisterPage} />
         <Route path="/signin" component={LoginPage} />
         <Route path="/cart/:id?" component={CartPage} />
         <Route path="/product/:id" component={ProductPage} />
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,4 +1,7 @@
 import {
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
   USER_SIGNIN_FAIL,
   USER_SIGNIN_REQUEST,
   USER_SIGNIN_SUCCESS,
@@ -6,7 +9,7 @@ import {
 } from "../constants/userConstants";
 
 export const userSigninReducer = (
-  state = { loading: false, userInfo: {} },
+  state = {},
   action
 ) => {
   switch (action.type) {
@@ -20,6 +23,24 @@ export const userSigninReducer = (
         userInfo: action.payload,
       };
     case USER_SIGNIN_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    case USER_SIGNOUT:
+      return {};
+    default:
+      return state;
+  }
+};
+
+export const userRegisterReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_REGISTER_REQUEST:
+      return { loading: true };
+    case USER_REGISTER_SUCCESS:
+      return { loading: false, userInfo: action.payload };
+    case USER_REGISTER_FAIL:
       return {
         loading: false,
         error: action.payload,
